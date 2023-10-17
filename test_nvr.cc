7a00a51728c12bb0e1df5861b31f9f93ac21216f
elliot53 <1604244855@qq.com>
2023-10-17 10:33:50 +0800

    增加nvr单元测试

--- test_nvr.cc
+++ test_nvr.cc
@@ -11,9 +11,24 @@
 
 #include "NvrTool.hpp"
 
-int main()
+static void Help()
 {
-    NvrTool nvr;
+    std::string info = R"(
+
+        --help
+        
+        --list
+        --create
+        --delete
+    )";
+
+    std::cerr << info << std::endl;
+}
+
+NvrTool nvr;
+
+int List()
+{
     bool res = nvr.List();
     if (!res)
     {
@@ -22,6 +37,74 @@ int main()
     else 
     {
         std::cerr << "succes" << std::endl;
+    }
+
+    return 0;
+}
+
+int Create()
+{
+    NvrTool::NvrCommand_t cmd;
+    cmd.input_fmt = "rtsp";
+    cmd.input_uri = "rtsp://admin:a1234567@192.169.7.123:554";
+    cmd.input_timeout = 30;
+    cmd.input_retry = 15;
+    cmd.input_xspeed = 1.0;
+    cmd.record_name = "123";
+    cmd.record_count = 20;
+    cmd.record_duration = 10;
+    cmd.publish_fmt = "flv";
+    cmd.publish_uri = "rtmp://192.169.4.16:1935/live/123";
+    bool res = nvr.Create(cmd);
+    if (!res)
+    {
+        std::cerr << "failed to lsit" << std::endl;
+    }
+    else 
+    {
+        std::cerr << "succes" << std::endl;
+    }
+
+    return 0;
+}
+
+int Delete()
+{
+    bool res = nvr.Delete();
+    if (!res)
+    {
+        std::cerr << "failed to lsit" << std::endl;
+    }
+    else 
+    {
+        std::cerr << "succes" << std::endl;
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    std::string arg;
+    for (int i = 1; i < argc; i++)
+    {
+        arg = argv[i];
+        if (arg == "--list")
+        {
+            List();
+        }
+        else if (arg == "--create")
+        {
+            Create();
+        }
+        else if (arg == "--delete")
+        {
+            Delete();
+        }
+        else 
+        {
+            Help();
+        }
     }
 
     return 0;

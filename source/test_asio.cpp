1a42bc7c9ada91706b31050fd5ba7c907414735a
elliot53 <1604244855@qq.com>
2023-12-01 13:42:25 +0800

    update

--- source/test_asio.cpp
+++ source/test_asio.cpp
@@ -11,9 +11,8 @@
 #include "entry.hpp"
 #include "asio.hpp"
 
-int test_asio(Message& message)
+int asio_timer_hello()
 {
-    LOG(INFO) << "test asio begin..." << "\n";
     asio::io_context io;
 
     asio::steady_timer t(io, asio::chrono::seconds(5));
@@ -21,6 +20,29 @@ int test_asio(Message& message)
     t.wait();
 
     LOG(INFO) << "hello world" << "\n";
+    LOG(INFO) << "test asio end..." << "\n";
+
+    return 1;
+}
+
+int test_asio(Message& message)
+{
+    LOG(INFO) << "test asio begin..." << "\n";
+
+    std::map<std::string, std::function<int()>> cmd_map = {
+        {"asio-timer-hello", asio_timer_hello}
+    };
+
+    std::string cmd = message.message_pool[2];
+
+    auto it = cmd_map.find(cmd);
+    if (it != cmd_map.end())
+    {
+        it->second();
+        return 1;
+    }
+
+    LOG(ERROR) << "invalid command: " << cmd << "\n";
     LOG(INFO) << "test asio end..." << "\n";
 
     return 0;
